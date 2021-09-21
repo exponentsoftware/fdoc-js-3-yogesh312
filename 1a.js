c417cbe2ca3016c0f672043ae7edd0aec2ddb60d
yogesh <etpr.yogesh@gmail.com>
2021-09-21 18:44:33 +0530

    driving key to the student array

---
+++ 1a.js
@@ -0,0 +1,13 @@
+
+
+const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+
+const names = student[0];
+const scores = student[2];
+const skills = student[1];
+const jsScore = student[2][2];
+const reactScore =student[2][3];
+
+console.log(names, skills, scores)
+console.log(jsScore, reactScore)
+
